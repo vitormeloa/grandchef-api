6a7c586656e8b4814a595adff00d3f57a189fc1f
Vitor Melo <melo.vitor2001@gmail.com>
2024-09-19 09:50:18 -0300

    Set up Pest configurations

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,9 +11,10 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+uses(
+    Tests\TestCase::class,
+    Illuminate\Foundation\Testing\RefreshDatabase::class,
+)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
